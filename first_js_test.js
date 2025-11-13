3bd78ed87f68e7ddd117233ed4ac097e2a606ec6
c29cs1 <c29cs1@dalton.org>
2025-11-13 11:46:13 -0500

    added new journal entry

--- first_js_test.js
+++ first_js_test.js
@@ -4,8 +4,10 @@ fih.style.left = "0px"
 
 document.addEventListener("keypress",function(){
     console.log("fih")
-    fih.style.left = parseInt(fih.style.left) + 5 + "px"
-    if (parseInt(fih.style.left) > parseInt("500px")){
-        fih.style.left = parseInt(fih.style.left) - 10 + "px"
+    if (event.key==="a"){
+        fih.style.left = parseInt(fih.style.left) - 5 + "px"
+    }
+    if (event.key==="d"){
+        fih.style.left = parseInt(fih.style.left) + 5 + "px"
     }
 })

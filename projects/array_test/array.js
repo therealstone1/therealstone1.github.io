879ffa6825dc541ecfd928ed2ef2792148c7e94b
c29cs1 <c29cs1@dalton.org>
2026-02-09 12:47:42 -0500

    new project

--- projects/array_test/array.js
+++ projects/array_test/array.js
@@ -1,23 +1,33 @@
 let clicker = document.getElementById("clicker")
 let clicker2 = document.getElementById("clicker2")
-let train = document.getElementById("Train")
+let train = document.getElementById("train")
+
 people = ["Chae ","Karm ","Sid "]
+
+function makeTrain(){
+    train.innerHTML = " "
+    for(let i=0; i<people.length; i++){
+        console.log(people[i])
+        let car = document.createElement("div")
+        car.textContent = people[i]
+        car.classList.add("car")
+        train.appendChild(car)
+    }
+}
 function ADD(){
     let addition = document.getElementById("addition")
     let addItem = addition.value
     if (addItem){
         people.push(addItem)
-        let cart = document.body.train.createElement("div")
-        cart.classList.add("car")
-        document.body.train.appendChild(cart)
-        console.log(people)
         addition.value = " "
+        makeTrain()
     }
 
 }
 function SUBTRACT(){
     people.pop()
-    console.log(people)
+    makeTrain()
 }
+makeTrain()
 clicker.addEventListener("click",ADD)
 clicker2.addEventListener("click",SUBTRACT)
